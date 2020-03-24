80c66ab31401365d05a9836c1b0ed9fec1d9abb8
popokobe <yuki.n3kobe@gmail.com>
2020-03-24 23:52:16 +0900

    ナビゲーションバーを作成

---
+++ components/NavigationBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app-bar
+    :clipped-left="$vuetify.breakpoint.lgAndUp"
+    app
+    color="blue darken-3"
+    dark
+  >
+    <v-app-bar-nav-icon @click.stop="$store.dispatch('showDrawer')" />
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+      <v-btn href="/" text>comida</v-btn>
+    </v-toolbar-title>
+    <v-text-field
+      flat
+      solo-inverted
+      hide-details
+      prepend-inner-icon="mdi-magnify"
+      label="検索"
+      class="hidden-sm-and-down"
+    />
+    <v-spacer />
+    <v-btn href="/login" text>
+      <v-icon>mdi-account</v-icon>
+      ログイン
+    </v-btn>
+    <v-btn href="/register" text>
+      <v-icon>mdi-account-circle</v-icon>
+      登録
+    </v-btn>
+    <v-btn href="/myaccount" text>
+      <v-icon>mdi-account</v-icon>
+      マイページ
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
