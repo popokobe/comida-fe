37042ad694fe7844157c679e44383d6c362fd50c
popokobe <yuki.n3kobe@gmail.com>
2020-03-25 14:57:14 +0900

    snackbarを追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,35 @@
 <template>
   <v-app id="comida">
-    <v-content>
-      <NavigationDrawer />
-      <NavigationBar />
+    <NavigationDrawer />
+    <NavigationBar />
 
+    <v-content>
       <v-container class="fill-height" fluid>
         <nuxt />
       </v-container>
       <Footer />
     </v-content>
+    <v-snackbar
+      v-for="(snackbar, index) in snackbars.filter((s) => s.showing)"
+      :key="snackbar.text + Math.random()"
+      :value="snackbar.showing"
+      :timeout="snackbar.timeout"
+      :color="snackbar.color"
+      :style="`bottom: ${index * 60 + 8}px`"
+      @input="removeSnackbar(snackbar)"
+    >
+      {{ snackbar.text }}
+
+      <v-btn text @click="removeSnackbar(snackbar)">
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-app>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import NavigationDrawer from '@/components/NavigationDrawer'
 import NavigationBar from '@/components/NavigationBar'
 import Footer from '@/components/Footer'
@@ -22,6 +39,16 @@ export default {
     NavigationDrawer,
     NavigationBar,
     Footer
+  },
+  computed: {
+    ...mapState({
+      snackbars: (state) => state.snackbar.snackbars
+    })
+  },
+  methods: {
+    removeSnackbar(snackbar) {
+      this.$store.dispatch('snackbar/remove', snackbar)
+    }
   }
 }
 </script>
