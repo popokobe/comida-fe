66453399ca1be3aab2b9306ff460e71b32bae232
popokobe <yuki.n3kobe@gmail.com>
2020-04-16 17:51:09 +0900

    新規投稿作成をできるように

--- components/PostRegisterForm.vue
+++ components/PostRegisterForm.vue
@@ -8,72 +8,55 @@
       </template>
       <v-card>
         <v-card-title>
-          <span class="headline">新規作成</span>
+          <span class="headline">お店を追加</span>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Legal middle name"
-                  hint="example of helper text only on focus"
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12" sm="6" md="4">
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-col>
               <v-col cols="12">
-                <v-text-field label="Email*" required></v-text-field>
+                <v-text-field v-model="name" label="お店の名前"></v-text-field>
               </v-col>
-              <v-col cols="12">
-                <v-text-field
-                  label="Password*"
-                  type="password"
-                  required
-                ></v-text-field>
+              <v-col cols="12" sm="4">
+                <v-text-field v-model="area" label="エリア"></v-text-field>
               </v-col>
-              <v-col cols="12" sm="6">
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-col>
-              <v-col cols="12" sm="6">
-                <v-autocomplete
-                  :items="[
-                    'Skiing',
-                    'Ice hockey',
-                    'Soccer',
-                    'Basketball',
-                    'Hockey',
-                    'Reading',
-                    'Writing',
-                    'Coding',
-                    'Basejump'
-                  ]"
-                  label="Interests"
+              <v-col cols="12" sm="4">
+                <!-- <v-autocomplete
+                  v-model="category"
+                  :items="categories"
+                  label="カテゴリ"
                   multiple
-                ></v-autocomplete>
+                ></v-autocomplete> -->
+                <v-text-field
+                  v-model="category"
+                  label="カテゴリ"
+                ></v-text-field>
+              </v-col>
+              <v-col v-model="expense" cols="12" sm="4">
+                <v-text-field label="予算"></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="dish" label="料理名"></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field v-model="address" label="住所"></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-textarea
+                  v-model="note"
+                  outlined
+                  label="メモ"
+                  hint="気になったことを書いてみよう！"
+                ></v-textarea>
               </v-col>
             </v-row>
           </v-container>
-          <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="blue darken-1" text @click="dialog = false"
             >閉じる</v-btn
           >
-          <v-btn color="blue darken-1" text @click="dialog = false">保存</v-btn>
+          <v-btn color="blue darken-1" text @click="submitPost">保存</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -84,7 +67,31 @@
 export default {
   data() {
     return {
-      dialog: false
+      dialog: true,
+      name: 'テスト',
+      area: 'テスト',
+      category: 'FA',
+      expense: 1000,
+      dish: 'テスト',
+      address: 'テスト',
+      note: 'テスト'
+    }
+  },
+  methods: {
+    async submitPost() {
+      await this.$axios
+        .$post('/api/v0/posts/', {
+          author_id: 1,
+          name: this.name,
+          area: this.area,
+          category: this.category,
+          expense: this.expense,
+          dish: this.dish,
+          address: this.address,
+          rating: 5,
+          note: this.note
+        })
+        .then((this.dialog = false))
     }
   }
 }
